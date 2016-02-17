05bc91668967a4294aa6968f3085890840e7fa74
hvelarde <hector.velarde@gmail.com>
2016-02-17 18:02:46 -0200

    Add Brazilian Portuguese and Spanish translations
    closes #23

--- src/collective/behavior/featuredimage/controlpanel.py
+++ src/collective/behavior/featuredimage/controlpanel.py
@@ -8,8 +8,8 @@ from plone.formwidget.namedfile.widget import NamedImageFieldWidget
 class PackageSettingsEditForm(controlpanel.RegistryEditForm):
 
     schema = IPackageSettings
-    label = _(u'Featured Image Settings')
-    description = _(u'Here you can modify the settings for collective.behavior.featuredimage.')
+    label = _(u'Featured Image')
+    description = _(u'Settings of the Featured Image.')
 
     def updateFields(self):
         """Update logo widget."""
