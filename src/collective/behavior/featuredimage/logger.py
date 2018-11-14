08ae7c0c37f334a2ba092df1c2ceeff2f5302240
hvelarde <hector.velarde@gmail.com>
2018-11-14 16:22:04 -0200

    Avoid ugly failures on featured image generation errors
    Give better feedback of the problem and store a copy of the whole page to help on debugging.

---
+++ src/collective/behavior/featuredimage/logger.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from collective.behavior.featuredimage.config import PROJECTNAME
+
+import logging
+
+
+logger = logging.getLogger(PROJECTNAME)
--- src/collective/behavior/featuredimage/subscriber.py
+++ src/collective/behavior/featuredimage/subscriber.py
@@ -1,15 +1,21 @@
 # coding: utf-8
+from collective.behavior.featuredimage import _
+from collective.behavior.featuredimage.logger import logger
 from cStringIO import StringIO
 from PIL import Image
+from plone import api
 from plone.namedfile.file import NamedBlobImage
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 
 import signal
 import transaction
 
 
-def _get_screenshot(page):
-    """Get screenshot of Fetured Image"""
+def _get_screenshot(page, request):
+    """Get a screenshot of the fetured image. Do not fail in case of
+    errors, but store an image of the whole page.
+    """
     browser = webdriver.PhantomJS()
     browser.implicitly_wait(1)  # seconds
 
@@ -17,25 +23,37 @@ def _get_screenshot(page):
     browser.set_window_size(1300, 1300)
     browser.get(page)
     data = browser.get_screenshot_as_png()
-    el = browser.find_element_by_id('featuredimage')
-    location, size = el.location, el.size
 
-    # XXX: quit() does not terminate PhantomJS process
-    #      https://github.com/SeleniumHQ/selenium/issues/767
-    browser.service.process.send_signal(signal.SIGTERM)
-    browser.quit()
+    location = size = None
+    try:
+        el = browser.find_element_by_id('featuredimage')
+        location, size = el.location, el.size
+    except NoSuchElementException:
+        logger.error('An error ocurred while generating the featured image')
+        msg = _(
+            'featured_image_generation_error',
+            default=u'An error ocurred while generating the featured image. '
+                    u'Please revert the workflow state. '
+                    u'The image generated can be used for further analysis.')
+        api.portal.show_message(msg, request=request, type='error')
+    finally:
+        # XXX: quit() does not terminate PhantomJS process
+        #      https://github.com/SeleniumHQ/selenium/issues/767
+        browser.service.process.send_signal(signal.SIGTERM)
+        browser.quit()
 
-    # crop image
     im = Image.open(StringIO(data))
-    im = im.crop((
-        location['x'], location['y'],
-        location['x'] + size['width'],
-        location['y'] + size['height']
-    ))
+    if location and size:  # crop image
+        im = im.crop((
+            location['x'], location['y'],
+            location['x'] + size['width'],
+            location['y'] + size['height']
+        ))
     output = StringIO()
     im.save(output, 'PNG', optimize=True)
     data = output.getvalue()
     output.close()
+
     return data
 
 
@@ -43,14 +61,15 @@ def update_featuredimage(context, event):
     """Update Fetured Image after save content"""
     if not context.featuredimage_enabled:
         return
-    page = u'{0}/@@featuredimage'.format(context.absolute_url())
+    page = '{0}/@@featuredimage'.format(context.absolute_url())
     # integration tests don't work with phantomjs
     if page.startswith('http://nohost/'):
         return
     # without this phantomjs look the old page
     transaction.commit()
     # get screenshot
-    data = _get_screenshot(page)
+    logger.info('Getting screenshot for: ' + page)
+    data = _get_screenshot(page, context.REQUEST)
     # save image
     image = NamedBlobImage()
     image._setData(data)
